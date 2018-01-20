70ecfad297673875006c23b9f9569abb8374b060
Tyto <theparanoxia@gmail.com>
2018-01-20 01:28:31 +0000

    bug fixes
    added some more awaits to stop blocking
    added timeout handling incase API is slow/down

--- cryptoprice/cryptoprice.py
+++ cryptoprice/cryptoprice.py
@@ -17,7 +17,6 @@ class CryptoPrice:
 
 	@commands.command(name='price', pass_context=True)
 	async def price(self, ctx, coin, returnCurr='USD', ex='all'):
-
 		"""Checks current price and 24hr percentage change of a coin. Optionally specify the return currency and the exchange (eg GDAX)."""
 		coin = coin.upper()
 		returnCurr = returnCurr.upper()
@@ -31,20 +30,21 @@ class CryptoPrice:
 
 		#Get dictionary of coin info, then try to get the price (p) and 24hr change (c24h) from the API.
 		try:
-			print("Trying to get coin from API")
-			dict = price.get_current_price(coinSearch, returnCurr, e=ex, try_conversion=True, full=True, format='display')
+			print("Trying to get " + coinSearch + " from API")
+			dict = await price.get_current_price(coinSearch, returnCurr, e=ex, try_conversion=True, full=True, format='display')
 			print("Trying to get price from returned JSON")
-			p = dict[coinSearch][returnCurr]['PRICE'].replace(" ", "")
+			p = await dict[coinSearch][returnCurr]['PRICE'].replace(" ", "")
 			print("Trying to get 24hr change")
-			c24h = dict[coinSearch][returnCurr]['CHANGEPCT24HOUR']
+			c24h = await dict[coinSearch][returnCurr]['CHANGEPCT24HOUR']
 			print("Got everything, saying message")
 			await self.bot.say("The current price of {} is {} ({}%)".format(coin,p,c24h))
 		except KeyError as e:
 			print("KeyError. Can't find data!", e)
 			await self.bot.say("Something went wrong. Check console log for more information.")
-			
-		return
-	
+		except TimeoutError as e:
+			print("Timeout. API might be slow/down.", e)
+			await self.bot.say("Timeout trying to contact cryptocompare API. Might be down/slow. Try again later?")
+
 	@commands.command(name='rsi', pass_context=True)
 	async def rsi(self, ctx):
 		"""Grabs top 5 oversold coins from RSIHunter"""
